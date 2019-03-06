7441b2f45dfe4d013f458efeca7a845d463111c5
simont1 <88simont@gmail.com>
2019-03-06 02:28:26 -0500

    K07 complete

--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -1,31 +1,67 @@
 #Team cookie: Angela Tom & Simon Tsui
 #SoftDev pd7
-#K #06: Yummy Mongo Py
-#2/28/19
+#K07 -- Import/Export Bank
+#2019-03-05
 
-#We chose the historical events database which includes the dates and description of recorded events
-#Raw data source:http://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en
+'''
+
+The dataset is on the US population.
+
+'''
 
 import pymongo
+import json
 from pymongo import MongoClient
+
 client = MongoClient()
 SERVER_ADDR = "142.93.207.20"
 connection = MongoClient(SERVER_ADDR)
-db = connection.historicalevents
-collection = db.history
-def findDate(year):
-    results = []
-    print("Hi")
-    #returns events that occurred in that year
-    for event in collection.find({"date": year}):
-        print(event)
-        results.append(event["description"])
-    return results
-print(findDate("-40"))
-def findCat(cat):
-    results = []
-    #returns events that occurred in that category
-    for event in collection.find({"category2": cat}):
-        results.append(event["description"])
-    return results
-print(findCat("Roman Republic"))
+db = connection.cookie
+collection = db.population
+
+'''
+The dataset is the United States population in 2010.
+Contents:
+   - females
+   - country
+   - age
+   - males
+   - year
+   - total
+
+Hyperlink to the raw data: http://api.population.io/1.0/population/2010/United%20States/?format=json
+
+Import mechanism: We loaded the json file and inserted it into the collection
+
+'''
+
+# import the json file
+def import_json():
+    with open("population.json") as p:
+        j = json.load(p)
+        collection.insert_many(j)
+
+# search for age
+def search_age(age):
+    a = collection.find({"age": age})
+    for x in a:
+        print (x)
+
+# searches for less than the number given
+def search_females(females):
+    f = collection.find({"females": {"$lt": females}})
+    for x in f:
+        print(f)
+
+# searches for less than the number given
+def search_males(males):
+    m = collection.find({"males": {"$lt": males}})
+    for x in m:
+        print(m)
+
+        
+
+import_json()
+#search_age(0)
+search_age(3)
+#search_females(50031)
