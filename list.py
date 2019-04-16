ba783949537c532ce565667d19e51c0cfa0df6ae
simont1 <88simont@gmail.com>
2019-04-15 22:26:08 -0400

    a

--- list.py
+++ list.py
@@ -1,79 +1,17 @@
-# azrael -- William Lu, Simon Tsui
-
-
-def oneA():
-    return [str(x * 2)*2 for x in range(5)]
-def oneB():
+def pyTriples(a):
     arr = []
-    for x in range(5):
-        arr.append(str(x * 2) * 2)
+    for m in range(1, a):
+        for n in range(1, a):
+            if m * n * 2 > 0 and m**2 - n** 2 > 0 and m**2 + n**2 > 0:
+                arr.append((m**2-n**2, 2*m*n, m**2 + n**2))
     return arr
+def quickSort(arr):
+    if arr == []:
+        return []
+    pivot = arr[0]
+    lower = quickSort([x for x in arr[1:] if x < pivot])
+    upper = quickSort([x for x in arr[1:] if x >= pivot])
+    return lower + [pivot] + upper
 
-def twoA():
-    arr = []
-    for x in range(5):
-        arr.append(x * 10 + 7)
-    return arr
-
-def twoB():
-    return [x * 10 + 7 for x in range(5)]
-
-def threeA():
-    arr = []
-    for x in range(3):
-        for y in range(3):
-            arr.append(x * y)
-    return arr
-
-def threeB():
-    return [x * y for x in range(3) for y in range(3)]
-
-def fourA():
-    arr = []
-    for i in range(2, 101):
-        for j in range(2, i/2 + 1):
-            if( i % j == 0):
-                arr.append(i)
-                break
-    return arr
-
-def isComp(x):
-    for j in range(2, x/2 + 1):
-        if x % j == 0:
-            return True
-    return False
-
-def fourB():
-    arr = [i if isComp(i)  else -1 for i in range(2, 101) ]
-    while -1 in arr:
-        arr.remove(-1)
-    return arr
-
-def fiveA():
-    arrComp = fourA()
-    arr = []
-    for i in range(2, 101):
-        if i not in arrComp:
-            arr.append(i)
-    return arr
-    
-def fiveB():
-    arr = [i if not isComp(i) else -1 for i in range(2, 101) ]
-    while -1 in arr:
-        arr.remove(-1)
-    return arr
-
-def sixA(x):
-    arr = []
-    for i in range(1, x+1):
-        if (x % i == 0):
-            arr.append(i)
-    return arr
-
-def sixB(x):
-    arr = [i if x % i == 0 else -1 for i in range(1, x + 1)]
-    while -1 in arr:
-        arr.remove(-1)
-    return arr
-
-print(sixB(100))
+print(pyTriples(10))
+print(quickSort([1,5,3,7,8,2,3,7,1,4,7,2,20]))
