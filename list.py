0a7ed03a929c1517f6a0f5ff41b85cb660084d7b
simont1 <stsui2@stuy.edu>
2019-04-16 13:06:47 -0400

    a

--- list.py
+++ list.py
@@ -1,17 +1,34 @@
-def pyTriples(a):
-    arr = []
-    for m in range(1, a):
-        for n in range(1, a):
-            if m * n * 2 > 0 and m**2 - n** 2 > 0 and m**2 + n**2 > 0:
-                arr.append((m**2-n**2, 2*m*n, m**2 + n**2))
-    return arr
-def quickSort(arr):
-    if arr == []:
-        return []
-    pivot = arr[0]
-    lower = quickSort([x for x in arr[1:] if x < pivot])
-    upper = quickSort([x for x in arr[1:] if x >= pivot])
-    return lower + [pivot] + upper
+#Team Bob - Simon Tsui, Jerry Ye
 
-print(pyTriples(10))
-print(quickSort([1,5,3,7,8,2,3,7,1,4,7,2,20]))
+
+arr1 = [1, 2, 3]
+arr2 = [2, 3, 4]
+arr3 = [4, 5, 6]
+arr4 = [2, 4, 6]
+#Union of sets A and B {1, 2, 3} & {2, 3, 4} --> {1, 2, 3, 4}
+
+def union(a, b):
+    return a + [i for i in b if i not in a]#adds set a to all items from set b not in a 
+
+print(union(arr1, arr2))
+
+#Intersection of sets A and B {1, 2, 3} & {2, 3, 4} --> {2, 3}
+def intersection(a, b):
+    return [i for i in a if i in b]#list comprehension of all items in a that are also in b 
+
+print(intersection(arr1, arr2))
+
+def setDifference(a,b):
+    return [i for i in a if i not in b] #list comprehension of all items in a not in b
+
+print(setDifference([1,2,3],[2,3,4]))
+print(setDifference([2,3,4], [1,2,3]))
+
+def symmetricDifference(a,b):
+    return [x for x in a + b if x not in intersection(a,b)]#exclusive or of sets a and b
+print(symmetricDifference(arr1,arr2))
+
+def cartesianProduct(a,b):
+    return [(r,c) for r in a for c in b]#all possible tuple combinations of sets a and b
+print(cartesianProduct(arr1,arr2))
+
